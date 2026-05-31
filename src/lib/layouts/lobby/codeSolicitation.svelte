5d04d0992c408a42545e0d57ff2cb064c0540b92
J. Lucas Hoffman <contato.jlhoffman@gmail.com>
2026-05-31 20:27:53 -0300

    refactor: change code solicitation div to form for better semantics and functionality

--- src/lib/layouts/lobby/codeSolicitation.svelte
+++ src/lib/layouts/lobby/codeSolicitation.svelte
@@ -2,9 +2,9 @@
     import IconButton from "$lib/components/buttons/iconButton.svelte";
 </script>
 
-<div id="code_solicitation" class="user_role_config">
-    <input type="text" placeholder="insert code">
+<form id="code_solicitation" class="user_role_config">
+    <input type="text" placeholder="insert code" name="code">
     <div id="code_submit">
         <IconButton icon="send" />
     </div>
-</div>
+</form>
