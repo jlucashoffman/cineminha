6ed083b052c89eeaed85fa9f664c55b12c035761
J. Lucas Hoffman <contato.jlhoffman@gmail.com>
2026-05-31 19:21:56 -0300

    feat: add room creation form for host users in lobby page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import CodeSolicitation from "$lib/layouts/lobby/codeSolicitation.svelte";
+    import RoomCreation from "$lib/layouts/lobby/roomCreation.svelte";
     import UserRoleSelection from "$lib/layouts/lobby/userRoleSelection.svelte";
     import { controller } from "$lib/types/store.svelte";
 </script>
@@ -7,6 +8,7 @@
 <main>
     <UserRoleSelection />
     {#if controller.lobbyUserRole == "guest"}
-    <CodeSolicitation />
-    {/if}
+    <CodeSolicitation /> {/if}
+    {#if controller.lobbyUserRole == "host"}
+    <RoomCreation /> {/if}
 </main>
