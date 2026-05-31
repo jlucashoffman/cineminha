865f2c72da65db37ebb82bb30064617482b864a3
J. Lucas Hoffman <contato.jlhoffman@gmail.com>
2026-05-31 18:58:42 -0300

    feat: add code solicitation component to lobby page for guest users

---
+++ src/lib/layouts/lobby/codeSolicitation.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import IconButton from "$lib/components/buttons/iconButton.svelte";
+</script>
+
+<div id="code_solicitation">
+    <input type="text" placeholder="insert code">
+    <div id="code_submit">
+        <IconButton icon="send" />
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+    import CodeSolicitation from "$lib/layouts/lobby/codeSolicitation.svelte";
     import UserRoleSelection from "$lib/layouts/lobby/userRoleSelection.svelte";
+    import { controller } from "$lib/types/store.svelte";
 </script>
 
 <main>
     <UserRoleSelection />
+    {#if controller.lobbyUserRole == "guest"}
+    <CodeSolicitation />
+    {/if}
 </main>
