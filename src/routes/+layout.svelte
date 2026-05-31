bd1540a8795bcaec74d6b7b9498830e00eaf2118
J. Lucas Hoffman <contato.jlhoffman@gmail.com>
2026-05-31 17:30:35 -0300

    feat: add Atkinson Hyperlegible font styles and integrate into layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
 	import favicon from '$lib/assets/favicon.svg';
 	import '$lib/styles/reset.css';
 	import '$lib/styles/neoreset.css';
+	import '$lib/styles/fonts.css';
+	import '$lib/styles/main.css';
 
 	let { children } = $props();
 </script>
