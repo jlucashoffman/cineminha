29ddb74633f6c593331036cada59240dba6a66da
J. Lucas Hoffman <contato.jlhoffman@gmail.com>
2026-05-31 21:17:43 -0300

    feat: add roomCode to AppStates and update navigation logic in layout

--- src/lib/types/store.svelte.ts
+++ src/lib/types/store.svelte.ts
@@ -2,6 +2,7 @@ export type userRole = "host" | "guest";
 
 class AppStates {
     lobbyUserRole?:userRole = $state();
+    roomCode?:number = $state();
 }
 
 export const controller = new AppStates();
--- src/routes/room/+layout.svelte
+++ src/routes/room/+layout.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
     import { page } from "$app/state";
+    import { controller } from "$lib/types/store.svelte";
     import { onMount } from "svelte";
 
     const roomId = $derived(page.params.id);
 
     onMount(() => {
-        if (!roomId) goto("/")
+        if (!roomId || !controller.roomCode || !controller.lobbyUserRole) goto("/")
     })
 </script>
