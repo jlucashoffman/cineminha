5fad04a74ff1316be555f8e290e28f223d8e8ea3
J. Lucas Hoffman <contato.jlhoffman@gmail.com>
2026-05-31 21:16:06 -0300

    feat: add basic room logic

---
+++ src/routes/room/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import { page } from "$app/state";
+    import { onMount } from "svelte";
+
+    const roomId = $derived(page.params.id);
+
+    onMount(() => {
+        if (!roomId) goto("/")
+    })
+</script>
