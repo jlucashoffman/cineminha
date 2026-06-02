0aa9d7913698ff0a16a85bf84ce41de0cb4da6c3
J. Lucas Hoffman <contato.jlhoffman@gmail.com>
2026-06-01 22:24:47 -0300

    fix: add children to layout.svelte room

--- src/routes/room/+layout.svelte
+++ src/routes/room/+layout.svelte
@@ -2,11 +2,16 @@
     import { goto } from "$app/navigation";
     import { page } from "$app/state";
     import { controller } from "$lib/types/store.svelte";
-    import { onMount } from "svelte";
+    import { onMount, type Snippet } from "svelte";
+
+    let {children}:{children:Snippet} = $props();
 
     const roomId = $derived(page.params.id);
 
     onMount(() => {
         if (!roomId || !controller.roomCode || !controller.lobbyUserRole) goto("/")
     })
+
 </script>
+
+{@render children?.()}
